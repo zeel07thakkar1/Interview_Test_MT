f71ba0731b242fb5c3f1fa0d321a31c41ffafb1e
chirag <chirag.creart@gmail.com>
2020-08-01 13:26:40 +0530

    Updation in Script file & process file

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($) {
   $('.method_checked_card').css('visibility', 'visible');
 
   
-
+  // --------Check email & password in modal-----
   $('.signin_btn').click(function(event) {
     
     var email = $('.input_email').val();
@@ -25,22 +25,21 @@ jQuery(document).ready(function($) {
 
   });
 
-  
 
-
-
+  // ----------show password---------
   $('.show_password').click(function(event) {
-    var pass = $('.input_pass');
+      var pass = $('.input_pass');
 
-    if((pass).attr('type', 'password'))
-    {
-        pass.attr('type', 'text');
-        $(this).css('display', 'none');
-        $('.hide_password').css('display', 'block');
-    }
-  });
+      if((pass).attr('type', 'password'))
+      {
+          pass.attr('type', 'text');
+          $(this).css('display', 'none');
+          $('.hide_password').css('display', 'block');
+      }
+    });
 
 
+  //-------hide password-----------------------
    $('.hide_password').click(function(event) {
         var pass = $('.input_pass');
 
@@ -50,9 +49,10 @@ jQuery(document).ready(function($) {
             $(this).css('display', 'none');
             $('.show_password').css('display', 'block');
         }
-        
-   });
+    });
 
+
+   //-------select payment method div-------------
    $('.pay_m_card').click(function(event) {
     
     $(this).addClass('payment_method_active');
@@ -90,116 +90,132 @@ jQuery(document).ready(function($) {
    });
 
 
-$('.select_plan_btn').click(function(event) {
-  
-  $('#plan_container').hide();
-  $('#acc_container').fadeIn('slow');
-  $('.acc_item , .plan_item').css('border-top', '2px solid #2ad694');
-  $(this).parentsUntil('html').find('.nav-item .radio').eq(1).addClass('radio_active');
-});
-
-
-$('.cont_btn_acc_cont').click(function(event) {
+//-------select plan---------------------------
+  $('.select_plan_btn').click(function(event) {
     
-    var email = $('.input_email').val();
-    var pass = $('.input_pass').val();
-    if(email != "" && pass !="")
-    {
-      $('#acc_container').hide();
-      $('#pay_container').fadeIn('slow');
-      $('.pay_item').css('border-top', '2px solid #2ad694');
-      $(this).parentsUntil('html').find('.nav-item .radio').eq(2).addClass('radio_active');
+    $('#plan_container').hide();
+    $('#acc_container').fadeIn('slow');
+    $('.acc_item , .plan_item').css('border-top', '2px solid #2ad694');
+    $(this).parentsUntil('html').find('.nav-item .radio').eq(1).addClass('radio_active');
+  });
+
+  //-----account container continue btn--------
+  $('.cont_btn_acc_cont').click(function(event) {
       
-    }
-    
-});
+      var email = $('.input_email').val();
+      var pass = $('.input_pass').val();
+      if(email != "" && pass !="")
+      {
+        $('#acc_container').hide();
+        $('#pay_container').fadeIn('slow');
+        $('.pay_item').css('border-top', '2px solid #2ad694');
+        $(this).parentsUntil('html').find('.nav-item .radio').eq(2).addClass('radio_active');
+        
+      }
+    });
 
 
-  $('.plan_item').click(function(event) {
-    $('#plan_container').fadeIn();
-    $('#acc_container').hide();
-    $('#pay_container').hide();
-  });
+  //-------plan tab click-----------------------
+  $('.process_link').eq(0).click(function(event) 
+  {
+      const parent= $(this).parents('.nav');
 
-  $('.acc_item').click(function(event) {
-    $('#plan_container').hide();
-    $('#acc_container').fadeIn();
-    $('#pay_container').hide();
-  });
+        $(this).siblings('.radio').addClass('radio_active');
+        parent.find('.nav-item .radio').eq(1).removeClass('radio_active');
+        parent.find('.nav-item .radio').eq(2).removeClass('radio_active');
 
-  $('.pay_item').click(function(event) {
-    $('#plan_container').hide();
-    $('#acc_container').hide();
-    $('#pay_container').fadeIn();
-  });
+        $(this).parent('.nav-item').css('border-top', '2px solid #2ad694');
+        parent.find('.nav-item').eq(1).css('border-top', '2px solid #4b4f53');
+        parent.find('.nav-item').eq(2).css('border-top', '2px solid #4b4f53');
 
-
-
-$('.process_link').eq(0).click(function(event) 
-{
-      $(this).siblings('.radio').addClass('radio_active');
-      $(this).parents('.nav').find('.nav-item .radio').eq(1).removeClass('radio_active');
-      $(this).parents('.nav').find('.nav-item .radio').eq(2).removeClass('radio_active');
-
-      $(this).parent('.nav-item').css('border-top', '2px solid #2ad694');
-      $(this).parents('.nav').find('.nav-item').eq(1).css('border-top', '2px solid #4b4f53');
-      $(this).parents('.nav').find('.nav-item').eq(2).css('border-top', '2px solid #4b4f53');
-
-});
-
-
-$('.process_link').eq(1).click(function(event) 
-{
-     $(this).parents('.nav').find('.nav-item .radio').eq(0).addClass('radio_active');
-     $(this).siblings('.radio').addClass('radio_active');
-     $(this).parents('.nav').find('.nav-item .radio').eq(2).removeClass('radio_active');
-
-      $(this).parent('.nav-item').css('border-top', '2px solid #2ad694');
-      $(this).parents('.nav').find('.nav-item').eq(0).css('border-top', '2px solid #2ad694');
-      $(this).parents('.nav').find('.nav-item').eq(2).css('border-top', '2px solid #4b4f53');
+        $('#plan_container').fadeIn();
+        $('#acc_container').hide();
+        $('#pay_container').hide();
 
   });
 
-$('.process_link').eq(2).click(function(event) 
-{
-      $(this).parents('.nav').find('.nav-item .radio').eq(0).addClass('radio_active');
-      $(this).parents('.nav').find('.nav-item .radio').eq(1).addClass('radio_active');
-      $(this).siblings('.radio').addClass('radio_active');
+  //------account tab click------------------
+  $('.process_link').eq(1).click(function(event) 
+  {
+      const parent= $(this).parents('.nav');
 
-      $(this).parent('.nav-item').css('border-top', '2px solid #2ad694');
-      $(this).parents('.nav').find('.nav-item').eq(0).css('border-top', '2px solid #2ad694');
-      $(this).parents('.nav').find('.nav-item').eq(1).css('border-top', '2px solid #2ad694');
-});
+       parent.find('.nav-item .radio').eq(0).addClass('radio_active');
+       $(this).siblings('.radio').addClass('radio_active');
+       parent.find('.nav-item .radio').eq(2).removeClass('radio_active');
+
+        $(this).parent('.nav-item').css('border-top', '2px solid #2ad694');
+        parent.find('.nav-item').eq(0).css('border-top', '2px solid #2ad694');
+        parent.find('.nav-item').eq(2).css('border-top', '2px solid #4b4f53');
+
+            $('#plan_container').hide();
+            $('#acc_container').fadeIn();
+            $('#pay_container').hide();
+
+    });
+
+  //---------payment tab click------------------
+  $('.process_link').eq(2).click(function(event) 
+  {
+       
+        var email = $(this).parentsUntil('html').find('#acc_container .input_email').val();
+        var pass = $(this).parentsUntil('html').find('#acc_container .input_pass').val();
+
+        const parent= $(this).parents('.nav');
+
+       if(email == "" && pass =="")
+        {
+            alert("You can't Go Dirctly to Payment!!! Please Fill the E-mail & Password");
+            $('#plan_container, #pay_container').hide();
+            $('#acc_container').fadeIn();
+
+            parent.find('.nav-item .radio').eq(0).addClass('radio_active');
+            parent.find('.nav-item .radio').eq(1).addClass('radio_active');
+
+            parent.find('.nav-item').eq(0).css('border-top', '2px solid #2ad694');
+            parent.find('.nav-item').eq(1).css('border-top', '2px solid #2ad694');
+
+        }
+        else
+        {
+            $('#plan_container, #acc_container').hide();
+            $('#pay_container').fadeIn();
+
+
+            parent.find('.nav-item .radio').eq(0).addClass('radio_active');
+            parent.find('.nav-item .radio').eq(1).addClass('radio_active');
+            $(this).siblings('.radio').addClass('radio_active');
+
+            $(this).parent('.nav-item').css('border-top', '2px solid #2ad694');
+            parent.find('.nav-item').eq(0).css('border-top', '2px solid #2ad694');
+            parent.find('.nav-item').eq(1).css('border-top', '2px solid #2ad694');
+        }
+  });
 
  
-  
-$('.back_link').click(function(event) 
-{
-    $('#creditcard_container,#crypto_container').hide();
-    $('#pay_container').fadeIn('slow');
-  
+  //--------back link click------------
+  $('.back_link').click(function(event) 
+  {
+      $('#creditcard_container,#crypto_container').hide();
+      $('#pay_container').fadeIn('slow');
+    
+  });
+
+
 });
 
+
+//------function which display the price which call on <a> tag in html file-------
 function display_prc (price) 
 {
-   var p=price;
-  var prc1=$('#creditcard_container').find('.navy_div .selected_plan_prc');
-  var prc2=$('#creditcard_container').find('.pay_btn .selected_plan_prc');
-  var prc3=$('#crypto_container').find('.navy_div .selected_plan_prc');
-  prc1.text(p);
-  prc2.text(p);
-  prc3.text(p);
 
+  $('#creditcard_container').find('.navy_div .selected_plan_prc, .pay_btn .selected_plan_prc').text(price);
+  $('#crypto_container').find('.navy_div .selected_plan_prc').text(price);
+ 
 }
 
-  $('.plan_btn_129').click(function(event) 
-  {
-     display_prc(129);
-  });
+function onTabNavigation (pageInfo) {
+  
+  $('#creditcard_container').hide();
+  $('#crypto_container').hide();
 
-  $('.plan_btn_249').click(function(event) 
-  {
-    display_prc(249);
-  });
-
-});
+}
